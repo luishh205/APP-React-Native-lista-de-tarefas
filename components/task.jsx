e728325604c9659c2410498134dc72a8b0583c8a
Luis Henrique Ramos Soares <89323852+luishh205@users.noreply.github.com>
2025-04-25 19:46:47 -0300

    Fnalizado app

--- components/task.jsx
+++ components/task.jsx
@@ -1,12 +1,15 @@
 import { View, StyleSheet, Pressable, Text } from "react-native"
 import { colors } from "../constants/colors"
 import { Ionicons } from "@expo/vector-icons"
+import { useState } from "react";
 
-export default function Task({ text , completed }) {
+export default function Task({ text , Initialcompleted }) {
+
+    const [completed, setCompleted] = useState(Initialcompleted)
 
     return (
         <View style={styles.rowContainer}>
-           <Pressable>
+           <Pressable onPress={() => setCompleted(!completed)}>
             <Ionicons name={"checkmark-circle"} size={32} color={completed ? colors.primary : "gray"} />
            </Pressable>
            <Text>{text}</Text>
